0a527b4aa098685d6031aad1313f235c542c1afe
Albert MS <a.m.serra8@gmail.com>
2021-05-28 14:49:13 +0200

    Implementada a funcionalidade que faz aumentar o tamano da cobra quando come e gera uma nova comida em outra posição aleatória

--- script.js
+++ script.js
@@ -8,24 +8,24 @@ context.fillRect(x, y, width, height)
 
 */
 
+//Inicializações
 let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 let box = 32;
+let direction = "right";
 
-let foodOnPlace = false;
-
+//Objeto cobrinha
 let snake = [];
 snake[0] = {
   x: 8 * box,
   y: 8 * box,
 };
 
+//Objeto comida. Cria as coordenadas de maneira aleatòria
 let food = {
-  x: 8 * box,
-  y: 8 * box,
+  x: Math.floor(Math.random() * 16) * box,
+  y: Math.floor(Math.random() * 16) * box,
 };
-
-let direction = "right";
 
 //Função para criar o campo de jogo
 function createBG() {
@@ -43,20 +43,17 @@ function createSnake() {
 
 //Função para criar a comida
 function placeFood() {
-  if (foodOnPlace == false) {
-    food.x = Math.floor(Math.random() * 16) * box;
-    food.y = Math.floor(Math.random() * 16) * box;
-    foodOnPlace = true;
-  }
   context.fillStyle = "orange";
   context.fillRect(food.x, food.y, box, box);
 }
 
+//Adicionando eventos para os botões de direções
 document.addEventListener("keydown", update);
 document.addEventListener("keyup", update);
 document.addEventListener("keyleft", update);
 document.addEventListener("keyright", update);
 
+//Função para mudar direção. Ela é chamada quando um dos botões de direção é apertado
 function update(event) {
   if (event.keyCode == 37 && direction != "right") direction = "left";
   if (event.keyCode == 38 && direction != "down") direction = "up";
@@ -64,34 +61,48 @@ function update(event) {
   if (event.keyCode == 40 && direction != "up") direction = "down";
 }
 
+//Função para iniciar o jogo
 function startGame() {
+  //Chama as funções para criar o campo de jogo, a cobrinha e a comida.
   createBG();
   createSnake();
   placeFood();
 
-  //console.log(snake[0].x, snake[0].y);
-
+  //Variavéis para guardar a posição atual da cobrinha
   let snakeX = snake[0].x;
   let snakeY = snake[0].y;
 
+  //Dependendo da direção modifica as variavéis das coordenadas da cobra
   if (direction == "right") snakeX += box;
   if (direction == "left") snakeX -= box;
   if (direction == "down") snakeY += box;
   if (direction == "up") snakeY -= box;
 
-  snake.pop();
+  //Tira a última parte da cobra (posição da array snake) no caso que as coordenadas sejam diferentes
+  if (snakeX != food.x || snakeY != food.y) {
+    snake.pop();
+  } else {
+    food.x = Math.floor(Math.random() * 16) * box;
+    food.y = Math.floor(Math.random() * 16) * box;
+  }
 
+  //Objeto para recever as coordenadas atualizadas da cabeça da cobrinha
   let newHead = {
     x: snakeX,
     y: snakeY,
   };
 
+  //Adiciona as coordenadas atuais da cabeça da cobra na array snake
   snake.unshift(newHead);
 
+  //console.log(snakeX, snakeY);
+
+  //Condições para fazer sair a cobra no lado oposto do campo no caso que ela sair do límite
   if (snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
   if (snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
   if (snake[0].x < 0 && direction == "left") snake[0].x = 15 * box;
   if (snake[0].y < 0 && direction == "up") snake[0].y = 15 * box;
 }
 
-let jogo = setInterval(startGame, 500);
+//Intervalo de execução da função startGame
+let jogo = setInterval(startGame, 100);
